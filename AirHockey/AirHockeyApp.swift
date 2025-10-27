e0b39e1e52b15851bf0be8541619f6b18320aa01
Neyilanau <halapovalbek@gmail.com>
2025-10-27 13:49:40 +0300

    Initial Commit

---
+++ AirHockey/AirHockeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirHockeyApp.swift
+//  AirHockey
+//
+//  Created by Альбек Халапов on 27.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct AirHockeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
